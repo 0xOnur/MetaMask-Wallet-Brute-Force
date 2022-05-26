022d52bb1793d9eb489ce2871943c2b8e11689cb
Onur <62797963+0xOnur@users.noreply.github.com>
2022-05-27 01:14:05 +0300

    Update checker.py

--- checker.py
+++ checker.py
@@ -1,112 +1,63 @@
-import requests
-import json
-from termcolor import colored, cprint
-import winsound
+import threading
 from hdwallet import BIP44HDWallet
 from hdwallet.cryptocurrencies import EthereumMainnet
 from hdwallet.derivations import BIP44Derivation
+from hdwallet.utils import generate_mnemonic
 from typing import Optional
+# from  etherscan import Etherscan
+import requests
+import json
+from termcolor import colored, cprint
+import time
+import os
 
-PASSPHRASE: Optional[str] = None
+ETHAPI = 'key'
+BSCAPI = 'key'
 
-bscAPI = "key"
-ethAPI = "key"
-avaxAPI = "key"
-polyAPI = "key"
+while True:    
+    MNEMONIC: str = generate_mnemonic(language="english", strength=128)
+    PASSPHRASE: Optional[str] = None  # "meherett"
+    bip44_hdwallet: BIP44HDWallet = BIP44HDWallet(cryptocurrency=EthereumMainnet)
+    bip44_hdwallet.from_mnemonic(
+        mnemonic=MNEMONIC, language="english", passphrase=PASSPHRASE
+    )
+    bip44_hdwallet.clean_derivation()
+    bip44_derivation: BIP44Derivation = BIP44Derivation(
+        cryptocurrency=EthereumMainnet, account=0, change=False, address=0)
+    bip44_hdwallet.from_path(path=bip44_derivation)
 
-duration = 2000
-freq = 900
+    me = bip44_hdwallet.mnemonic()
+    addr = bip44_hdwallet.address()
 
-count = 0
-with open("walletsBeCheck.txt", "r+") as file:
-    while True:
-        wallet = file.readline()
-        words = wallet.split(' ')
-        myMnemonic = words[0] + ' ' + words[1] + ' ' + words[2] + ' ' + words[3] + ' ' + words[4] + ' ' + words[5] + ' ' + words[6] + ' ' + words[7] + ' ' + words[8] + ' ' + words[9] + ' ' + words[10] + ' ' + words[11]
+    # #ETH
+    eth =requests.get(f'https://api.etherscan.io/api?module=account&action=txlist&address={addr}&startblock=0&endblock=99999999&page=1&offset=10&sort=asc&apikey={ETHAPI}')
+    ethJson =eth.json()
+    dumpETHJson = json.dumps(ethJson)
 
-        # Initialize Ethereum mainnet BIP44HDWallet
-        bip44_hdwallet: BIP44HDWallet = BIP44HDWallet(cryptocurrency=EthereumMainnet)
-        # Get Ethereum BIP44HDWallet from mnemonic
-        bip44_hdwallet.from_mnemonic(mnemonic=myMnemonic[:-1], language="english", passphrase=PASSPHRASE)
-        # Clean default BIP44 derivation indexes/paths
-        bip44_hdwallet.clean_derivation()
-        
-        # Get Ethereum BIP44HDWallet information's from address index
-        bip44_derivation: BIP44Derivation = BIP44Derivation(
-                cryptocurrency=EthereumMainnet, account=0, change=False, address=0)
-        bip44_hdwallet.from_path(path=bip44_derivation)
-        
-        
-        #API CHECK WALLETS
-            
-        #BSC
-        bsc = requests.get(f'https://api.bscscan.com/api?module=account&action=balance&address={bip44_hdwallet.address()}&apikey={bscAPI}')
-        bscJson = bsc.json()
-        dumpBSCJson = json.dumps(bscJson)
+    loadETHJson = json.loads(dumpETHJson)
+    ethTransaction = loadETHJson["status"]
+    if int(ethTransaction) >0:
+        print(colored(f"Has transaction history {me} {addr}", color="green"))
+        with open("valid.txt", "a") as ethWallets:
+                ethWallets.write("\nWallet: " + me + " ETH CHAIN " + addr)
+    else:
+        print(colored(f"{ethTransaction} {me} {addr}", color="red"))
 
-        loadBSCJson = json.loads(dumpBSCJson)
-        bscBalance = loadBSCJson["result"]
-        print("BSC:"+bscBalance)
-        if int(bscBalance) > 0:
-            print(colored("money", color="green"))
-            winsound.Beep(freq, duration)
-            with open("checkedWallets.txt", "a") as bscWallets:
-                bscWallets.write("\nWallet: " + wallet + " BSC CHAIN " + bip44_hdwallet.address())
-        else:
-            print(colored("nothing", color="red"))
+    #BSC
+    bsc =requests.get(f'https://api.bscscan.com/api?module=account&action=txlist&address={addr}&startblock=0&endblock=99999999&page=1&offset=10&sort=asc&apikey={BSCAPI}')
+    bscJson =bsc.json()
+    dumpBSCJson = json.dumps(bscJson)
 
-
-        #ETH
-        eth =requests.get(f'https://api.etherscan.io/api?module=account&action=balance&address={bip44_hdwallet.address()}&apikey={ethAPI}')
-        ethJson =eth.json()
-        dumpETHJson = json.dumps(ethJson)
-
-        loadETHJson = json.loads(dumpETHJson)
-        ethBalance = loadETHJson["result"]
-        print("ETH:"+ethBalance)
-        if int(ethBalance) > 0:
-            print(colored("money", color="green"))
-            winsound.Beep(freq, duration)
-            with open("checkedWallets.txt", "a") as ethWallets:
-                ethWallets.write("\nWallet: " + wallet + "ETH CHAIN " + bip44_hdwallet.address())
-        else:
-            print(colored("nothing", color="red"))
-
-        #AVAX
-        avax = requests.get(f'https://api.snowtrace.io/api?module=account&action=balance&address={bip44_hdwallet.address()}&apikey={avaxAPI}')
-        avaxJson = avax.json()
-        dumpAvaxJson = json.dumps(avaxJson)
-
-        loadAvaxJson = json.loads(dumpAvaxJson)
-        avaxBalance = loadAvaxJson['result']
-        print("AVAX:"+avaxBalance)
-        if int(avaxBalance) > 0:
-            print(colored("money", color="green"))
-            winsound.Beep(freq, duration)
-            with open("checkedWallets.txt", "a") as avaxWallets:
-                avaxWallets.write("\nWallet: " + wallet + "AVAX CHAIN " + bip44_hdwallet.address())
-        else:
-            print(colored("nothing", color="red"))
-            
-        #POLYGON
-        polygon = requests.get(f'https://api.polygonscan.com/api?module=account&action=balance&address={bip44_hdwallet.address()}&apikey={polyAPI}')
-        polyJson = polygon.json()
-        dumpPolyJson = json.dumps(polyJson)
-
-        loadPolyJson = json.loads(dumpPolyJson)
-        polyBalance = loadPolyJson['result']
-        print("MATİC:"+polyBalance)
-        if int(polyBalance) > 0:
-            print(colored("money", color="green"))
-            winsound.Beep(freq, duration)
-            with open("checkedWallets.txt", "a") as polyWallets:
-                polyWallets.write("\nWallet: " + wallet + "POLYGON CHAIN " + bip44_hdwallet.address())
-        else:
-            print(colored("nothing", color="red"))
+    loadBSCJson = json.loads(dumpBSCJson)
+    bscTransaction = loadBSCJson["status"]
+    if int(bscTransaction) >0:
+        print(colored(f"Has transaction history {me} {addr}", color="green"))
+        with open("valid.txt", "a") as bscWallets:
+                bscWallets.write("\nWallet: " + me + " BSC CHAIN " + addr)
+    else:
+        print(colored(f"{bscTransaction} {me} {addr}", color="red"))
 
 
-        count+=1
-        print(f'CHECKED:{count}')
-        print(f"{bip44_hdwallet.mnemonic()} {bip44_hdwallet.address()}")
-        print("")
-        bip44_hdwallet.clean_derivation()
+
+
+    bip44_hdwallet.clean_derivation()
